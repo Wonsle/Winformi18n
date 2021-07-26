9314ae3dd936b9cf0e1b48b31652b63e9d7bf36a
KunLinLee <kerberos8280@gmail.com>
2021-07-26 20:14:13 +0800

    新增切換語言按鈕、顯示文字

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,64 @@ namespace i18n
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnzh_TW = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnzh_TW
+            // 
+            this.btnzh_TW.Location = new System.Drawing.Point(87, 43);
+            this.btnzh_TW.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnzh_TW.Name = "btnzh_TW";
+            this.btnzh_TW.Size = new System.Drawing.Size(125, 36);
+            this.btnzh_TW.TabIndex = 0;
+            this.btnzh_TW.Text = "中文";
+            this.btnzh_TW.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(87, 110);
+            this.btn.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(125, 36);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "English";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(341, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "測試";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(432, 168);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btnzh_TW);
+            this.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnzh_TW;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
